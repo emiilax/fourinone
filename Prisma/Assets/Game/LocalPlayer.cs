6f5c1426130b3f8fd4ccbaf614df52817122824f
andreas berneryd <andreas berneryd>
2017-02-21 11:31:25 +0100

    Functional with cameras part of player prefab networkmanager auto spawns player

---
+++ Prisma/Assets/Game/LocalPlayer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class LocalPlayer : NetworkBehaviour
+{
+
+    // Use this for initialization
+    void Start()
+    {
+        Camera playerCamera = this.GetComponent<Camera>();
+        if (isLocalPlayer)
+        {
+            //Camera.main.enabled = false;
+            Debug.Log(Application.loadedLevelName);
+            Debug.Log("created player");
+            Debug.Log(playerCamera.transform.position);
+            //Destroy(playerCamera);
+            //GetComponent<MoveBall>().enabled = true;
+        }
+        else
+        {
+            Destroy(playerCamera);
+            Debug.Log("spawning other player");
+        }
+    }
+
+}
---
+++ Prisma/Assets/Game/PlayerSpawn.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class PlayerSpawn : NetworkBehaviour
+{
+    public Camera PlayerCamera;
+
+    void Start()
+    {
+        Debug.Log(Application.loadedLevelName);
+        Debug.Log("start spawn");
+        if (isLocalPlayer)
+        {
+            Debug.Log(Application.loadedLevelName);
+            Debug.Log("local start spawn");
+            //GetComponent<MoveBall>().enabled = true;
+
+        }
+    }
+
+    public override void OnStartClient() {
+        // base.OnStartLocalPlayer();
+        Debug.Log("Spawn script running");
+        //PlayerCamera.enabled = true;
+        //Debug.Log(PlayerCamera.gameObject.name);
+        if (isLocalPlayer) {
+            Debug.Log("is local client");
+            PlayerCamera.enabled = true;
+        }
+        
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
