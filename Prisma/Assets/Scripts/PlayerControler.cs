2d46bbd30ef7c77f59d9e5015facbae4f7da6fa6
Noak Rosengren <noakrosengren@Noaks-MacBook-Pro.local>
2017-03-11 07:04:24 +0100

    fixed laser origin

--- Prisma/Assets/Scripts/PlayerControler.cs
+++ Prisma/Assets/Scripts/PlayerControler.cs
@@ -38,19 +38,23 @@ public class PlayerControler : NetworkBehaviour {
 
 			if (isLocalPlayer) {
 				Vector3 mousePosition = playerCamera.ScreenToWorldPoint (Input.mousePosition);
-				Ray2D ray = new Ray2D (gameObject.transform.position, mousePosition);
+				Vector3 playerPosition = gameObject.transform.position;
+				Vector3 direction = new Vector3 (mousePosition.x-playerPosition.x, mousePosition.y-playerPosition.y, 0);
+				//Vector3 origin = new Vector3 (playerPosition.x, playerPosition.y, 0);
+				Ray2D ray = new Ray2D (playerPosition, direction);
 				RaycastHit2D hit = Physics2D.Raycast (gameObject.transform.position, mousePosition);
 				laser.numPositions = 2;
 				int ptNum = 1;
 				int maxBounces = 16;
 				int bounceNum = 0;
+
 				laser.SetPosition (ptNum, ray.GetPoint (100));
 
 				Vector3[] laserPositions = new Vector3[laser.numPositions];
 				laser.GetPositions (laserPositions);
 
 				foreach (Vector3 vector in laserPositions) {
-					Debug.Log("laserpositions" + vector.ToString ());
+					//Debug.Log("laserpositions" + vector.ToString ());
 				}
 
 				CmdSynchLaser (gameObject, laserPositions);
@@ -94,7 +98,7 @@ public class PlayerControler : NetworkBehaviour {
 	}
 	[Command]
 	void CmdSynchLaser(GameObject player, Vector3[] laserPos){
-		Debug.Log ("synch command");
+		//Debug.Log ("synch command");
 		RpcSynchLaser (player, laserPos);
 	}
 
