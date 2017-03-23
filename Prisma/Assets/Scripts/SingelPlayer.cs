32fd4d35ee60470a78347fe0242164f8fd0aa62b
Martin Tran <martin_t_39@hotmail.com>
2017-03-23 15:15:03 +0100

    Add singleplayer

---
+++ Prisma/Assets/Scripts/SingelPlayer.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SingelPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	/* Action handler for back-button to last scene*/
+	public void BackButtonPressed() {
+
+		SceneManager.LoadScene ("Lobby");
+		MyNetworkLobbyManager.singelton.gameObject.SetActive (true);
+
+	}
+}
