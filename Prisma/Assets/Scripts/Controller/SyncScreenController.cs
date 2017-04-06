b4810026f7e47a50806a8e7f26ef1aae4d8a6d5e
Martin Tran <martin_t_39@hotmail.com>
2017-04-06 16:05:58 +0200

    Add prototype syncscreen

---
+++ Prisma/Assets/Scripts/Controller/SyncScreenController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SyncScreenController : MonoBehaviour {
+
+	public static SyncScreenController instance;
+
+	void Awake() {
+		//If we don't currently have a game control...
+		if (instance == null)
+			//...set this one to be it...
+			instance = this;
+		//...otherwise...
+		else if(instance != this)
+			//...destroy this one because it is a duplicate.
+			Destroy (gameObject);
+	}
+		
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
