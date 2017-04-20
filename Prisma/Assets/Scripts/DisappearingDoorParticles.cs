d064bf5096a28210b14b64f8ca5b5be9f3b707af
henriktran <henrikttran@gmail.com>
2017-04-20 20:08:04 +0200

    Fixed particles, new playersprites and removed old sprites

--- Prisma/Assets/Scripts/DisappearingDoorParticles.cs
+++ Prisma/Assets/Scripts/DisappearingDoorParticles.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class testglow : MonoBehaviour {
+public class DisappearingDoorParticles : MonoBehaviour {
     public ParticleSystem particles;
     // Use this for initialization
     void Start () {
