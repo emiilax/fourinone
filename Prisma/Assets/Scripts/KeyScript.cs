8ee94b901d4e93ade43f08395a072fe5aee844d1
emiilax <e.axelsson@hotmail.se>
2017-04-25 10:23:25 +0200

    stuff start to work?

--- Prisma/Assets/Scripts/KeyScript.cs
+++ Prisma/Assets/Scripts/KeyScript.cs
@@ -1,12 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Networking;
 
-public class KeyScript : MonoBehaviour {
+public class KeyScript : NetworkBehaviour {
 
 	public GameObject door;	
 
+	public GameObject key;
+
 	public bool unlocked;
+
+	void Awake(){
+		Debug.Log ("I'm awake");
+
+		NetworkServer.Spawn (door);
+
+	}
+
+
+	public override void OnStartServer(){
+		Debug.Log ("now im active!");
+	}
+
 
 	void Start(){
 		unlocked = false;
